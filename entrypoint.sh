05b71859ac8584f5b23294e9d46500fb87d3c331
croconut <marsteve@ucsc.edu>
2020-07-18 18:09:11 -0700

    echo testing

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -l
 
-ls
-ls $GITHUB_WORKSPACE
-ls $GITHUB_HOME
+echo $(ls)
+echo $(ls $GITHUB_WORKSPACE)
+echo $(ls $GITHUB_HOME)
 godot -d -s addons/gut/gut_cmdln.gd -gdir=res://test -ginclude_subdirs -gexit
