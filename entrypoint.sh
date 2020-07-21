2e6a555c38d02fe3883b68063b598d22eb5a814a
croconut <marsteve@ucsc.edu>
2020-07-20 17:17:46 -0700

    hopefully fixes

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,11 +4,11 @@ GODOT_VERSION=$1
 PROJECT_DIRECTORY=$2
 GODOT_RELEASE_TYPE=$3
 GODOT_SERVER_TYPE="headless"
-CUSTOM_DL_PATH="~/customdls/"
+CUSTOM_DL_PATH="~/custom_dl_folder"
 
 FULL_GODOT_NAME=Godot_v${GODOT_VERSION}-${GODOT_RELEASE_TYPE}_linux_${GODOT_SERVER_TYPE}.64
 
-cd /${PROJECT_DIRECTORY}
+cd ./${PROJECT_DIRECTORY}
 
 # setup linux environment
 apt-get update && apt-get install -y --no-install-recommends ca-certificates git wget unzip zip curl python python-openssl
@@ -21,8 +21,9 @@ mkdir -p ${CUSTOM_DL_PATH}
 wget https://downloads.tuxfamily.org/godotengine/${GODOT_VERSION}/${FULL_GODOT_NAME}.zip -P ${CUSTOM_DL_PATH}
 mkdir -p ~/.cache
 mkdir -p ~/.config/godot
-unzip ${CUSTOM_DL_PATH}${FULL_GODOT_NAME}.zip
-rm -f ${CUSTOM_DL_PATH}${FULL_GODOT_NAME}.zip
+unzip ${CUSTOM_DL_PATH}/${FULL_GODOT_NAME}.zip -d ${CUSTOM_DL_PATH}
+rm -f ${CUSTOM_DL_PATH}/${FULL_GODOT_NAME}.zip
 
 # run tests
-${CUSTOM_DL_PATH}${FULL_GODOT_NAME} -d -s addons/gut/gut_cmdln.gd -gdir=res://test -ginclude_subdirs -gexit
+${CUSTOM_DL_PATH}/${FULL_GODOT_NAME} -d -s addons/gut/gut_cmdln.gd -gdir=res://test -ginclude_subdirs -gexit
+rm -f ${CUSTOM_DL_PATH}/${FULL_GODOT_NAME}
