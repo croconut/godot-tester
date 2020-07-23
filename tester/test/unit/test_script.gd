c33b2038b5f631f48bbc7a7b44aa344c3157cf33
croconut <marsteve@ucsc.edu>
2020-07-23 15:24:05 -0700

    a test to check for import resolution, i think

--- tester/test/unit/test_script.gd
+++ tester/test/unit/test_script.gd
@@ -1,4 +1,17 @@
 extends "res://addons/gut/test.gd"
 
+const main_scene = preload("res://main.tscn")
+var scene
+var current_scene
+
+func before_all():
+	var root = get_tree().get_root()
+	current_scene = root.get_child(root.get_child_count() -1)
+
 func test_assert_one_less_than_two():
 	assert_true(1 < 2)
+	
+func test_load_scene() ->void:
+	scene = main_scene.instance()
+	current_scene.add_child(scene)
+	assert_eq(scene.name, current_scene.get_node(scene.name).name)
