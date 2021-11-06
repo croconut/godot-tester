f53d88e760b6933d1b14715ae4e1335ff006635b
Matthew Stevens <52222076+croconut@users.noreply.github.com>
2021-11-06 10:50:17 -0700

    Alternate run mode (#10)
    * attempt one for alternate run mode
    
    * fix1
    
    * fix2
    
    * fix3
    
    * fix4
    
    * fix5
    
    * fix6
    
    * testing timeout removal
    
    * think i broke mono
    
    * unbroke mono
    
    * buttered toast
    
    * whoops
    
    * fixed path issue
    
    * read me update

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,13 @@
 
 set -e
 
+# credit: https://stackoverflow.com/questions/24283097/reusing-output-from-last-command-in-bash
+# capture the output of a command so it can be retrieved with ret
+cap () { tee /tmp/capture.out; }
+
+# return the output of the most recent command that was captured by cap
+ret () { cat /tmp/capture.out; }
+
 GODOT_VERSION=$1
 RELEASE_TYPE=$2
 PROJECT_DIRECTORY=$3
@@ -11,8 +18,11 @@ IMPORT_TIME=$5
 TEST_TIME=$6
 MINIMUM_PASSRATE=$7
 TEST_DIR=$8
+DIRECT_SCENE=$9
+
 GODOT_SERVER_TYPE="headless"
 CUSTOM_DL_PATH="~/custom_dl_folder"
+RUN_OPTIONS="-s addons/gut/gut_cmdln.gd -gdir=${TEST_DIR} -ginclude_subdirs -gexit"
 
 if [ "$RELEASE_TYPE" = "stable" ] ; then
     DL_PATH_SUFFIX=""
@@ -21,7 +31,7 @@ else
 fi
 
 # if download places changes, will need updates to this if/else
-if [ IS_MONO = "true" ] ; then
+if [ "$IS_MONO" = "true" ] ; then
     GODOT_RELEASE_TYPE="${RELEASE_TYPE}_mono"
     DL_PATH_EXTENSION="${GODOT_VERSION}${DL_PATH_SUFFIX}/mono/"
     GODOT_EXTENSION="_64"
@@ -32,6 +42,10 @@ else
     DL_PATH_EXTENSION="${GODOT_VERSION}${DL_PATH_SUFFIX}/"
     GODOT_EXTENSION=".64"
     FULL_GODOT_NAME=Godot_v${GODOT_VERSION}-${GODOT_RELEASE_TYPE}_linux_${GODOT_SERVER_TYPE}
+fi
+
+if [ "$DIRECT_SCENE" != "false" ] ; then
+    RUN_OPTIONS="${DIRECT_SCENE}"
 fi
 
 cd ./${PROJECT_DIRECTORY}
@@ -49,17 +63,19 @@ mkdir -p ~/.cache
 mkdir -p ~/.config/godot
 echo "unzipping ..."
 yes | unzip -q ${CUSTOM_DL_PATH}/${FULL_GODOT_NAME}${GODOT_EXTENSION}.zip -d ${CUSTOM_DL_PATH}
+chmod -R 777 ${CUSTOM_DL_PATH}
 
+echo "running test suites ..."
 
 set +e
 # run tests
-if [ IS_MONO = "true" ] ; then
+if [ "$IS_MONO" = "true" ] ; then
 # need to init the imports
-    outp0=$(timeout ${IMPORT_TIME} ${CUSTOM_DL_PATH}/${FULL_GODOT_NAME}${GODOT_EXTENSION}/${FULL_GODOT_NAME}.64 -e 2>&1)
-    outp=$(timeout ${TEST_TIME} ${CUSTOM_DL_PATH}/${FULL_GODOT_NAME}${GODOT_EXTENSION}/${FULL_GODOT_NAME}.64 -s addons/gut/gut_cmdln.gd -gdir=${TEST_DIR} -ginclude_subdirs -gexit 2>&1)
+    timeout ${IMPORT_TIME} ./${CUSTOM_DL_PATH}/${FULL_GODOT_NAME}${GODOT_EXTENSION}/${FULL_GODOT_NAME}.64 -e
+    timeout ${TEST_TIME} ./${CUSTOM_DL_PATH}/${FULL_GODOT_NAME}${GODOT_EXTENSION}/${FULL_GODOT_NAME}.64 ${RUN_OPTIONS} | cap
 else
-    outp0=$(timeout ${IMPORT_TIME} ${CUSTOM_DL_PATH}/${FULL_GODOT_NAME}${GODOT_EXTENSION} -e 2>&1)
-    outp=$(timeout ${TEST_TIME} ${CUSTOM_DL_PATH}/${FULL_GODOT_NAME}${GODOT_EXTENSION} -s addons/gut/gut_cmdln.gd -gdir=${TEST_DIR} -ginclude_subdirs -gexit 2>&1)
+    timeout ${IMPORT_TIME} ./${CUSTOM_DL_PATH}/${FULL_GODOT_NAME}${GODOT_EXTENSION} -e
+    timeout ${TEST_TIME} ./${CUSTOM_DL_PATH}/${FULL_GODOT_NAME}${GODOT_EXTENSION} ${RUN_OPTIONS} | cap
 fi
 
 rm -rf ${CUSTOM_DL_PATH}/${FULL_GODOT_NAME}${GODOT_EXTENSION}
@@ -100,7 +116,7 @@ while read line; do
             fi
         done
     fi
-done <<< "$(echo "${outp}")"
+done <<< `ret`
 
 # ensuring failing enough tests / being timed out cause failure for
 # the action
