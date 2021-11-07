d0b57251a5ff52200a1da8b5b899b03df11a402a
Matthew Stevens <52222076+croconut@users.noreply.github.com>
2021-11-06 21:23:18 -0700

    swap test to pending (#14)

--- tester/test/unit/test_script.gd
+++ tester/test/unit/test_script.gd
@@ -9,7 +9,8 @@ func before_all():
 	current_scene = root.get_child(root.get_child_count() -1)
 
 func test_assert_one_less_than_two():
-	assert_true(1 < 2)
+	# assert_true(1 < 2)
+	pending()
 
 func test_assert_oe_less_than_two():
 	assert_true(1 < 2)
@@ -39,7 +40,7 @@ func test_load_scene1() ->void:
 	current_scene.add_child(scene)
 	assert_true(11 < 4)
 	# should fail pretty hard
-	main_scene.blargh()
+#	main_scene.blargh()
 	
 	assert_eq(scene.name, current_scene.get_node(scene.name).name)
 
