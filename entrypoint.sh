99913aa1dacf27e70cd763de44acb81531ad6064
croconut <marsteve@ucsc.edu>
2021-11-06 19:48:50 -0700

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -193,7 +193,7 @@ else
     echo $MAX_FAILS
     echo $FAILED
 
-    if [[ "$MAX_FAILS" -ne 0 ]]; then
+    if [[ "$MAX_FAILS" != "false" ]]; then
         if [[ "$FAILED" -gt "$MAX_FAILS" ]]; then
             endmsg="Tests failed due to fail count of ${FAILED} exceeding maximum of ${MAX_FAILS}"
         fi
