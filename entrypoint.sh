01a5178a3b1927a3f80e096a2438a405e31d1e7a
Raul A. Gonzalez <info@rgonzalez.tech>
2023-05-14 01:10:15 -0700

    Extract behavior into named functions

--- entrypoint.sh
+++ entrypoint.sh
@@ -20,133 +20,19 @@ CONFIG_FILE=${13}
 CUSTOM_GODOT_DL_URL=${14}
 RESULT_OUTPUT_FILE=${15}
 
-GODOT_SERVER_TYPE="headless"
+# Internal variables
+exitval=1
+CUSTOM_DL_PATH="~/custom_dl_folder"
 TESTS=0
 FAILED=0
-CUSTOM_DL_PATH="~/custom_dl_folder"
-
-RUN_OPTIONS="-s addons/gut/gut_cmdln.gd"
-RUN_OPTIONS="${RUN_OPTIONS} -gdir=${TEST_DIR}"
-RUN_OPTIONS="${RUN_OPTIONS} -ginclude_subdirs"
-RUN_OPTIONS="${RUN_OPTIONS} -gjunit_xml_file=./${RESULT_OUTPUT_FILE}"
-RUN_OPTIONS="${RUN_OPTIONS} -gexit"
 
 # Import functions
 source ./lib/functions.sh
 
-if [ "$RELEASE_TYPE" = "stable" ]; then
-    DL_PATH_SUFFIX=""
-else
-    DL_PATH_SUFFIX="/${RELEASE_TYPE}"
-fi
-
-# if download places changes, will need updates to this if/else
-if [ "$IS_MONO" = "true" ]; then
-    GODOT_RELEASE_TYPE="${RELEASE_TYPE}_mono"
-    DL_PATH_EXTENSION="${GODOT_VERSION}${DL_PATH_SUFFIX}/mono/"
-    GODOT_EXTENSION="_64"
-    # this is a folder for mono versions
-    FULL_GODOT_NAME=Godot_v${GODOT_VERSION}-${GODOT_RELEASE_TYPE}_linux_${GODOT_SERVER_TYPE}
-else
-    GODOT_RELEASE_TYPE="${RELEASE_TYPE}"
-    DL_PATH_EXTENSION="${GODOT_VERSION}${DL_PATH_SUFFIX}/"
-    GODOT_EXTENSION=".64"
-    FULL_GODOT_NAME=Godot_v${GODOT_VERSION}-${GODOT_RELEASE_TYPE}_linux_${GODOT_SERVER_TYPE}
-fi
-
-# these are mutually exclusive - direct scenes cannot take a config file but they can
-# have all those options set on the scene itself anyways
-if [ "$DIRECT_SCENE" != "false" ]; then
-    RUN_OPTIONS="${DIRECT_SCENE}"
-elif [ "$CONFIG_FILE" != "res://.gutconfig.json" ]; then
-    RUN_OPTIONS="${RUN_OPTIONS} -gconfig=${CONFIG_FILE}"
-fi
-
 cd ./${PROJECT_DIRECTORY}
 
-mkdir -p ${CUSTOM_DL_PATH}
-mkdir -p ./addons/gut/.cli_add
-mv -n /__rebuilder.gd ./addons/gut/.cli_add
-mv -n /__rebuilder_scene.tscn ./addons/gut/.cli_add
-
-# in case this was somehow there already, but broken
-rm -rf ${CUSTOM_DL_PATH}/${FULL_GODOT_NAME}${GODOT_EXTENSION}
-rm -f ${CUSTOM_DL_PATH}/${FULL_GODOT_NAME}${GODOT_EXTENSION}.zip
-
-# setup godot environment
-DL_URL="https://downloads.tuxfamily.org/godotengine/${DL_PATH_EXTENSION}${FULL_GODOT_NAME}${GODOT_EXTENSION}.zip"
-if [ "$CUSTOM_GODOT_DL_URL" != "" ]; then
-    DL_URL="$CUSTOM_GODOT_DL_URL"
-fi
-
-echo "downloading godot from ${DL_URL} ..."
-yes | wget -q ${DL_URL} -P ${CUSTOM_DL_PATH}
-mkdir -p ~/.cache
-mkdir -p ~/.config/godot
-echo "unzipping ..."
-yes | unzip -q ${CUSTOM_DL_PATH}/${FULL_GODOT_NAME}${GODOT_EXTENSION}.zip -d ${CUSTOM_DL_PATH}
-chmod -R 777 ${CUSTOM_DL_PATH}
-
-echo "running test suites ..."
-
-set +e
-# run tests
-if [ "$IS_MONO" = "true" ]; then
-    # need to init the imports
-    # workaround for -e -q and -e with timeout failing
-    # credit: https://github.com/Kersoph/open-sequential-logic-simulation/pull/4/files
-    timeout ${IMPORT_TIME} ./${CUSTOM_DL_PATH}/${FULL_GODOT_NAME}${GODOT_EXTENSION}/${FULL_GODOT_NAME}.64 --editor addons/gut/.cli_add/__rebuilder_scene.tscn
-    timeout ${TEST_TIME} ./${CUSTOM_DL_PATH}/${FULL_GODOT_NAME}${GODOT_EXTENSION}/${FULL_GODOT_NAME}.64 ${RUN_OPTIONS} 2>&1 | cap
-else
-    timeout ${IMPORT_TIME} ./${CUSTOM_DL_PATH}/${FULL_GODOT_NAME}${GODOT_EXTENSION} --editor addons/gut/.cli_add/__rebuilder_scene.tscn
-    timeout ${TEST_TIME} ./${CUSTOM_DL_PATH}/${FULL_GODOT_NAME}${GODOT_EXTENSION} ${RUN_OPTIONS} 2>&1 | cap
-fi
-
-# removing scene used to rebuild import files
-rm -rf ./addons/gut/.cli_add/__rebuilder.gd
-rm -rf ./addons/gut/.cli_add/__rebuilder_scene.tscn
-
-rm -rf ${CUSTOM_DL_PATH}/${FULL_GODOT_NAME}${GODOT_EXTENSION}
-rm -f ${CUSTOM_DL_PATH}/${FULL_GODOT_NAME}${GODOT_EXTENSION}.zip
-
-# parsing test output to fill test count and pass count variables
-
-if [ "$ASSERT_CHECK" != "false" ]; then
-    check_by_assert
-else
-    check_by_test
-fi
-
-rm -f ./${RESULT_OUTPUT_FILE}
-
-passrate=".0"
-endmsg=""
-if [ "$TESTS" -eq "0" ]; then
-    exitval=1
-    endmsg="Tests failed due to timeout or there were no tests to run\n"
-else
-    passrate=$(echo "scale=3; ($TESTS-$FAILED)/$TESTS" | bc -l)
-    echo -e "\n${passrate} pass rate\n"
-    if (($(echo "$passrate >= $MINIMUM_PASSRATE" | bc -l))); then
-        exitval=0
-    else
-        endmsg="Tests failed due to low passrate\n"
-        exitval=1
-    fi
-
-    if [[ "$MAX_FAILS" != "false" ]]; then
-        if [[ "$FAILED" -gt "$MAX_FAILS" ]]; then
-            endmsg="Tests failed due to fail count of ${FAILED} exceeding maximum of ${MAX_FAILS}"
-            exitval=1
-        fi
-    fi
-fi
-
-if [ "$endmsg" != "" ]; then
-    echo -e "Note: Tests may appear to pass on SCRIPT ERROR, but they were just ignored by GUT"
-    echo -e "This action ensures those will fail"
-    echo
-    echo -e "${endmsg}"
-fi
+download_godot
+run_tests
+analyze_test_results
 
 exit ${exitval}
--- lib/functions.sh
+++ lib/functions.sh
@@ -9,6 +9,86 @@ ret() { cat /tmp/capture.out; }
 # https://stackoverflow.com/questions/893585/how-to-parse-xml-in-bash/2608159#2608159
 rdom () { local IFS=\> ; read -d \< E C ;}
 
+delete_godot() {
+    rm -rf ${CUSTOM_DL_PATH}/${FULL_GODOT_NAME_EXT}
+    rm -f ${CUSTOM_DL_PATH}/${FULL_GODOT_NAME_EXT}.zip
+}
+
+add_gut_rebuilder() {
+    mkdir -p ./addons/gut/.cli_add
+    mv -n /__rebuilder.gd ./addons/gut/.cli_add
+    mv -n /__rebuilder_scene.tscn ./addons/gut/.cli_add
+}
+
+delete_gut_rebuilder() {
+    rm -rf ./addons/gut/.cli_add/__rebuilder.gd
+    rm -rf ./addons/gut/.cli_add/__rebuilder_scene.tscn
+}
+
+generate_dl_url() {
+    if [ "$RELEASE_TYPE" = "stable" ]; then
+        GODOT_URL_PATH_SUBDIR=""
+    else
+        # if not stable, then it's a release candidate or beta
+        # Those are located in a subdirectory of the release type
+        # example: /beta1, /rc1, etc.
+        GODOT_URL_PATH_SUBDIR="/${RELEASE_TYPE}"
+    fi
+
+    # This is the path to the godot version hosted on tuxfamily
+    # example: 3.2.3/, 3.2.3/beta1/, etc.
+    GODOT_URL_PATH="${GODOT_VERSION}${GODOT_URL_PATH_SUBDIR}/"
+    if [ "$IS_MONO" = "true" ]; then
+        # mono builds are in a subdirectory of the godot version
+        # example: 3.2.3/mono/, 3.2.3/beta1/mono/, etc.
+        GODOT_URL_PATH="${GODOT_URL_PATH}mono/"
+        FULL_GODOT_NAME="Godot_v${GODOT_VERSION}-${RELEASE_TYPE}_mono_linux_headless"
+        FULL_GODOT_NAME_EXT="${FULL_GODOT_NAME}_64"
+    else
+        FULL_GODOT_NAME="Godot_v${GODOT_VERSION}-${RELEASE_TYPE}_linux_headless"
+        FULL_GODOT_NAME_EXT="${FULL_GODOT_NAME}.64"
+    fi
+
+    DL_URL="https://downloads.tuxfamily.org/godotengine/${GODOT_URL_PATH}${FULL_GODOT_NAME_EXT}.zip"
+
+    if [ "$CUSTOM_GODOT_DL_URL" != "" ]; then
+        DL_URL="$CUSTOM_GODOT_DL_URL"
+    fi
+}
+
+download_godot() {
+    # Defining these here so they can be used later in the function
+    generate_dl_url
+
+    # Just in case there was a broken godot from a previous run
+    delete_godot
+
+    mkdir -p ${CUSTOM_DL_PATH}
+    echo "downloading godot from ${DL_URL} ..."
+    yes | wget -q ${DL_URL} -P ${CUSTOM_DL_PATH}
+    mkdir -p ~/.cache
+    mkdir -p ~/.config/godot
+    echo "unzipping ..."
+    yes | unzip -q ${CUSTOM_DL_PATH}/${FULL_GODOT_NAME_EXT}.zip -d ${CUSTOM_DL_PATH}
+    chmod -R 777 ${CUSTOM_DL_PATH}
+}
+
+generate_run_options() {
+    RUN_OPTIONS="-s addons/gut/gut_cmdln.gd"
+    RUN_OPTIONS="${RUN_OPTIONS} -gdir=${TEST_DIR}"
+    RUN_OPTIONS="${RUN_OPTIONS} -ginclude_subdirs"
+    RUN_OPTIONS="${RUN_OPTIONS} -gjunit_xml_file=./${RESULT_OUTPUT_FILE}"
+    RUN_OPTIONS="${RUN_OPTIONS} -gexit"
+
+    # these are mutually exclusive - direct scenes cannot take a config file but they can
+    # have all those options set on the scene itself anyways
+    if [ "$DIRECT_SCENE" != "false" ]; then
+        RUN_OPTIONS="${DIRECT_SCENE}"
+    elif [ "$CONFIG_FILE" != "res://.gutconfig.json" ]; then
+        RUN_OPTIONS="${RUN_OPTIONS} -gconfig=${CONFIG_FILE}"
+    fi
+}
+
 check_by_test() {
     FAILURE_REGEX="failures=\"([0-9]+)\""
     TESTS_REGEX="tests=\"([0-9]+)\""
@@ -50,4 +130,69 @@ check_by_assert() {
 
     echo "XML: Asserts Detected: ${TESTS}"
     echo "XML: Failed Asserts: ${FAILED}"
+}
+
+calculate_pass_rate() {
+    passrate=".0"
+    endmsg=""
+    if [ "$TESTS" -eq "0" ]; then
+        exitval=1
+        endmsg="Tests failed due to timeout or there were no tests to run\n"
+    else
+        passrate=$(echo "scale=3; ($TESTS-$FAILED)/$TESTS" | bc -l)
+        echo -e "\n${passrate} pass rate\n"
+        if (($(echo "$passrate >= $MINIMUM_PASSRATE" | bc -l))); then
+            exitval=0
+        else
+            endmsg="Tests failed due to low passrate\n"
+            exitval=1
+        fi
+
+        if [[ "$MAX_FAILS" != "false" ]]; then
+            if [[ "$FAILED" -gt "$MAX_FAILS" ]]; then
+                endmsg="Tests failed due to fail count of ${FAILED} exceeding maximum of ${MAX_FAILS}"
+                exitval=1
+            fi
+        fi
+    fi
+
+    if [ "$endmsg" != "" ]; then
+        echo -e "Note: Tests may appear to pass on SCRIPT ERROR, but they were just ignored by GUT"
+        echo -e "This action ensures those will fail"
+        echo
+        echo -e "${endmsg}"
+    fi
+}
+
+run_tests() {
+    set +e
+
+    add_gut_rebuilder
+    generate_run_options
+
+    echo "running test suites ..."
+    if [ "$IS_MONO" = "true" ]; then
+        # need to init the imports
+        # workaround for -e -q and -e with timeout failing
+        # credit: https://github.com/Kersoph/open-sequential-logic-simulation/pull/4/files
+        timeout ${IMPORT_TIME} ./${CUSTOM_DL_PATH}/${FULL_GODOT_NAME_EXT}/${FULL_GODOT_NAME}.64 --editor addons/gut/.cli_add/__rebuilder_scene.tscn
+        timeout ${TEST_TIME} ./${CUSTOM_DL_PATH}/${FULL_GODOT_NAME_EXT}/${FULL_GODOT_NAME}.64 ${RUN_OPTIONS} 2>&1 | cap
+    else
+        timeout ${IMPORT_TIME} ./${CUSTOM_DL_PATH}/${FULL_GODOT_NAME_EXT} --editor addons/gut/.cli_add/__rebuilder_scene.tscn
+        timeout ${TEST_TIME} ./${CUSTOM_DL_PATH}/${FULL_GODOT_NAME_EXT} ${RUN_OPTIONS} 2>&1 | cap
+    fi
+
+    delete_gut_rebuilder
+    delete_godot
+}
+
+analyze_test_results() {
+    if [ "$ASSERT_CHECK" != "false" ]; then
+        check_by_assert
+    else
+        check_by_test
+    fi
+
+    rm -f ./${RESULT_OUTPUT_FILE}
+    calculate_pass_rate
 }
