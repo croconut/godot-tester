8fcb9edabd6c64c067f5fc61e756a3913950de06
Raul A. Gonzalez <info@rgonzalez.tech>
2023-06-28 23:37:45 -0700

    Run tests on index.js

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 #!/usr/bin/env node
 const downloadGodot = require('./lib/DownloadGodot/DownloadGodot');
+const runGodotImport = require('./lib/RunGodotImport/RunGodotImport');
+const executeGutTests = require('./lib/ExecuteGutTests/ExecuteGutTests');
 
 async function runTests() {
     process.env.THIS_ACTION_DIR = __dirname;
 
     const exePath = await downloadGodot();
     console.log('exePath: ', exePath);
+
+    runGodotImport(exePath);
+    executeGutTests(exePath);
+    // TODO: analyze test results
+    // analyzeTestResults(); 
 };
 
 runTests();
