1f1ab6f5253092534100be7edccde0a8cc7dc059
Raul A. Gonzalez <info@rgonzalez.tech>
2023-06-29 22:47:16 -0700

    Run the analyzer in index

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const downloadGodot = require('./lib/DownloadGodot/DownloadGodot');
 const runGodotImport = require('./lib/RunGodotImport/RunGodotImport');
 const executeGutTests = require('./lib/ExecuteGutTests/ExecuteGutTests');
+const analyzeTestResults = require('./lib/AnalyzeTestResults/AnalyzeTestResults');
 
 async function runTests() {
     process.env.THIS_ACTION_DIR = __dirname;
@@ -11,8 +12,16 @@ async function runTests() {
 
     runGodotImport(exePath);
     executeGutTests(exePath);
-    // TODO: analyze test results
-    // analyzeTestResults(); 
+    const results = analyzeTestResults(); 
+
+    console.log("Test count: ", results.testCount);
+    console.log("Fail count: ", results.failCount);
+    console.log("Passrate: ", results.passRate);
+    console.log("Success: ", results.success);
+
+    if(!results.success) {
+        process.exit(1);
+    };
 };
 
 runTests();
